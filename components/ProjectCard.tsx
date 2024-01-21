b9896bce249aac6a8cc6eb74aeea61c084849ec5
Nathan Fredericks <nathan@fredericks.dev>
2024-01-21 09:59:56 -0400

    Add projects and change grid

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -11,13 +11,13 @@ type Props = {
 export default function ProjectCard({Icon, name, description, tags, link}: Props) {
   const card = (
     <>
-      <Icon className="h-12 w-12"/><h1 className="my-2 text-2xl font-bold text-gray-900">{name}</h1><p
-        className="mb-3 font-medium text-gray-800">{description}</p>
+      <Icon className="h-12 w-12"/><h1 className="my-2 text-2xl font-bold text-gray-900">{name}</h1>
+      <p className="font-medium text-gray-800">{description}</p>
       <div className="flex flex-row flex-wrap">
         {tags.map((tag) => (
           <span
             key={tag}
-            className="me-2 mt-1 whitespace-nowrap rounded-full border border-gray-800 bg-white px-2.5 py-1 font-mono text-xs font-bold text-gray-800 md:mt-2"
+            className="me-2 mt-2 whitespace-nowrap rounded-full border border-gray-800 bg-white px-2.5 py-1 font-mono text-xs font-bold text-gray-800 md:mt-2"
           >
             {tag}
           </span>
--- sections/ProjectsSection.tsx
+++ sections/ProjectsSection.tsx
@@ -1,6 +1,6 @@
 import {
   CommandLineIcon,
-  NewspaperIcon,
+  NewspaperIcon, PencilIcon,
   UserIcon,
   WrenchIcon,
 } from '@heroicons/react/24/solid';
@@ -10,26 +10,33 @@ import Section from '@/sections/Section';
 export default function ProjectsSection() {
   return (
     <Section id="projects" Icon={CommandLineIcon} name="Projects">
-      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
         <ProjectCard
           Icon={UserIcon}
           name="Portfolio Website"
-          description="I designed and developed my personal website from scratch using NextJS and Tailwind CSS."
+          description="I designed and developed my personal website from scratch using NextJS and Tailwind CSS. The website was designed with readability and page speed in mind."
           tags={['NextJS', 'Tailwind CSS']}
           link="https://github.com/nathanfredericks/nathanfredericks.github.io"
         />
         <ProjectCard
           Icon={WrenchIcon}
           name="PocketMine Tools"
-          description="I built a website for Minecraft server administrators and plugin developers. The website offers 9 responsive utilities for efficient server administration and plugin development."
+          description="I built a website for Minecraft server administrators and plugin developers. The website offers 9 responsive utilities for efficient server administration and plugin development. The project has been in active development since 2019."
           tags={['NextJS', 'Bootstrap']}
           link="https://github.com/nathanfredericks/pocketmine-tools"
         />
         <ProjectCard
           Icon={NewspaperIcon}
           name="HN Now"
-          description="Using early betas of SwiftUI, I developed HN Now. A Hacker News reader for iOS and iPadOS."
-          tags={['iOS', 'Swift', 'SwiftUI']}
+          description="Using early betas of SwiftUI, I developed HN Now. A Hacker News reader for iOS and iPadOS. HN Now was published to the Apple App Store in 2020."
+          tags={['Swift', 'SwiftUI']}
+          link="https://github.com/nathanfredericks/HNNow"
+        />
+        <ProjectCard
+          Icon={PencilIcon}
+          name="Nathan's Stickies"
+          description="I built a script and website to sync my desktop sticky notes to the cloud. I used this project to improve my knowlege of AppleScript and Django, and solve a problem."
+          tags={['Python', 'Django', 'AppleScript']}
           link="https://github.com/nathanfredericks/HNNow"
         />
       </div>
